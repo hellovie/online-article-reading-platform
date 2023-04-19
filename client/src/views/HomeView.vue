d211e5ee6f0bcf511a56f180106d7b246b086f42
hellovie <hellovie@foxmail.com>
2023-04-19 11:19:49 +0800

    feat(client): 搭建vite+vue3前端开发环境

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+
+const counter = useCounterStore()
+const count = () => {
+  counter.increment()
+}
+</script>
+
+<template>
+  <div>
+    <h2>在线文章阅读系统</h2>
+    Counter: <button @click="count()">{{ counter.count }}</button>
+  </div>
+</template>
+
+<style scoped>
+button {
+  padding: 1px 20px;
+}
+</style>
